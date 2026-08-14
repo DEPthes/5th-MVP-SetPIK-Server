fe76636d519082fc93035cd50c4e60be1739e8c6
jungeyop <jungyeop61@gmail.com>
2026-08-14 16:27:58 +0900

    chore: 캘린더 기능 제거

--- src/main/java/com/setpik/server/common/exception/ErrorCode.java
+++ src/main/java/com/setpik/server/common/exception/ErrorCode.java
@@ -15,7 +15,6 @@ public enum ErrorCode {
 	SPOTIFY_API_ERROR(HttpStatus.BAD_GATEWAY, 2200, "Spotify API 처리에 실패했습니다."),
 	ANALYSIS_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 2300, "플레이리스트 분석에 실패했습니다."),
 	PERFORMANCE_MATCH_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 2400, "공연 매칭에 실패했습니다."),
-	CALENDAR_ENTRY_DUPLICATED(HttpStatus.CONFLICT, 2500, "이미 등록된 캘린더 일정입니다."),
 	PRESTUDY_PLAYLIST_CREATION_FAILED(HttpStatus.BAD_GATEWAY, 2600, "예습 플레이리스트 생성에 실패했습니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 3000, "서버 내부 오류가 발생했습니다.");
 
